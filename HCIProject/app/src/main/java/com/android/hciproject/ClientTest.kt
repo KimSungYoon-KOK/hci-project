76e9cd5fd49ad1721764cd5da435c35297df244a
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-05-26 15:43:26 +0900

    aws_amplify setting

---
+++ HCIProject/app/src/main/java/com/android/hciproject/ClientTest.kt
@@ -0,0 +1,38 @@
+package com.android.hciproject
+
+import android.content.Context
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
+import com.amazonaws.mobileconnectors.appsync.sigv4.CognitoUserPoolsAuthProvider
+
+
+class ClientTest {
+    @Volatile
+    private var client: AWSAppSyncClient? = null
+
+    @Synchronized
+    fun init(context: Context?) {
+        if (client == null) {
+            val awsConfiguration = AWSConfiguration(context)
+            client = context?.let {
+                AWSAppSyncClient.builder()
+                    .context(it)
+                    .awsConfiguration(awsConfiguration)
+                    .cognitoUserPoolsAuthProvider {
+                        try {
+                            AWSMobileClient.getInstance().tokens.idToken.tokenString
+                        } catch (e: Exception) {
+                            e.localizedMessage
+                        }
+                    }.build()
+            }
+        }
+    }
+
+    @Synchronized
+    fun appSyncClient(): AWSAppSyncClient? {
+        return client
+    }
+}
+
