7ded638e7afc5c42135817510cbcd1da3f02aa76
KimSungYoon-KOK <sungyun1997@gmail.com>
2021-06-03 19:11:22 +0900

    commit

--- HCIProject/app/src/main/java/com/android/hciproject/testAmplify/AuthenticationActivity.kt
+++ HCIProject/app/src/main/java/com/android/hciproject/testAmplify/AuthenticationActivity.kt
@@ -1,11 +1,7 @@
 package com.android.hciproject.testAmplify
 
-import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.amazonaws.mobile.client.*
 import com.android.hciproject.R
 
 
@@ -14,44 +10,7 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
-        AWSMobileClient.getInstance()
-            .initialize(this, object : Callback<UserStateDetails?> {
-                override fun onResult(userStateDetails: UserStateDetails?) {
-                    if (userStateDetails != null) {
-                        Log.i(TAG, userStateDetails.userState.toString())
-
-                        when (userStateDetails.userState) {
-                            UserState.SIGNED_IN -> {
-                                val i = Intent(this@AuthenticationActivity, TestActivity::class.java)
-                                startActivity(i)
-                                finish()
-                            }
-                            UserState.SIGNED_OUT -> showSignIn()
-                            else -> {
-                                AWSMobileClient.getInstance().signOut()
-                                showSignIn()
-                            }
-                        }
-                    }
-                }
-
-                override fun onError(e: Exception) {
-                    Log.e(TAG, e.toString())
-                }
-
-            })
     }
 
-    private fun showSignIn() {
-        try {
-            AWSMobileClient.getInstance()
-                .showSignIn(
-                this,
-                SignInUIOptions.builder().nextActivity(TestActivity::class.java).build()
-            )
-            finish()
-        } catch (e: java.lang.Exception) {
-            Log.e(TAG, e.toString())
-        }
-    }
+
 }
--- HCIProject/app/src/main/java/com/android/hciproject/ui/LoginFragment.kt
+++ HCIProject/app/src/main/java/com/android/hciproject/ui/LoginFragment.kt
@@ -1,17 +1,17 @@
 package com.android.hciproject.ui
 
-import android.content.Context
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import androidx.navigation.fragment.findNavController
-import com.android.hciproject.R
+import androidx.fragment.app.Fragment
+import com.amazonaws.mobile.client.*
 import com.android.hciproject.databinding.LoginFragmentBinding
+import com.android.hciproject.testAmplify.TestActivity
 import com.android.hciproject.viewmodels.LoginViewModel
-import com.google.android.material.snackbar.Snackbar
 
 class LoginFragment : Fragment() {
 
@@ -32,26 +32,63 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setLoginClickListener()
+//        setLoginClickListener()
+        AWSMobileClient.getInstance()
+            .initialize(this, object : Callback<UserStateDetails?> {
+                override fun onResult(userStateDetails: UserStateDetails?) {
+                    if (userStateDetails != null) {
+                        Log.i(ContentValues.TAG, userStateDetails.userState.toString())
+
+                        when (userStateDetails.userState) {
+                            UserState.SIGNED_IN -> {
+                                // 메인으로 이동
+//                                val i = Intent(require(context), TestActivity::class.java)
+//                                startActivity(i)
+                            }
+                            UserState.SIGNED_OUT -> showSignIn()
+                            else -> {
+                                AWSMobileClient.getInstance().signOut()
+                                showSignIn()
+                            }
+                        }
+                    }
+                }
+
+                override fun onError(e: Exception) {
+                    Log.e(ContentValues.TAG, e.toString())
+                }
+            })
     }
 
-    private fun setLoginClickListener() {
-        binding.loginButton.setOnClickListener {
-            login()
+    private fun showSignIn() {
+        try {
+            AWSMobileClient.getInstance()
+                .showSignIn(
+                    this,
+                    SignInUIOptions.builder().nextActivity(TestActivity::class.java).build()
+                )
+        } catch (e: java.lang.Exception) {
+            Log.e(ContentValues.TAG, e.toString())
         }
     }
 
-    private fun login() {
-        val imm =
-            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(binding.username.windowToken, 0)
-        viewModel.login()
-        findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
-        Snackbar.make(
-            binding.container,
-            "안녕하세요, "+binding.username.text.toString()+"님!",
-            Snackbar.LENGTH_SHORT
-        ).show()
-    }
+//    private fun setLoginClickListener() {
+//        binding.loginButton.setOnClickListener {
+//            login()
+//        }
+//    }
+//
+//    private fun login() {
+//        val imm =
+//            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(binding.username.windowToken, 0)
+//        viewModel.login()
+//        findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+//        Snackbar.make(
+//            binding.container,
+//            "안녕하세요, "+binding.username.text.toString()+"님!",
+//            Snackbar.LENGTH_SHORT
+//        ).show()
+//    }
 
 }
